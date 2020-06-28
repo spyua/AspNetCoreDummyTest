51a8eb366ad38bb5d8b0c9686f0d0aa1827a6834
I29042 <i29042@icsc.com.tw>
2020-06-28 15:45:12 +0800

    feat : SignarIR測試專案

---
+++ DummySignalIRWithJs/DummySignalIRWithJs/wwwroot/js/chat.js
@@ -0,0 +1,43 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+// Connect BackEnd and FrontEnd Init
+connection.start().then(function () {
+
+    // Connect成功
+    document.getElementById("sendButton").disabled = false;
+
+
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+
+connection.on("ReceiveMessage", function (user, message) {
+
+    // ChatHub 廣播
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+
+    li.textContent = encodedMsg;
+
+    document.getElementById("messagesList").appendChild(li);
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+
+    // Call ChatHub
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+    event.preventDefault();
+});
